8aaab829303a9a4cf16008d825a9dd8bb5323777
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-09-26 23:52:06 +0600

    refactor: Update server.ts to use environment variable for port

--- server.ts
+++ server.ts
@@ -5,12 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+  
   res.send('Hello World');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on:`);
+
+
   const networkInterfaces = os.networkInterfaces();
+
+
   Object.keys(networkInterfaces).forEach((interfaceName) => {
     networkInterfaces[interfaceName]?.forEach((iface) => {
       if (iface.family === 'IPv4' && !iface.internal) {
