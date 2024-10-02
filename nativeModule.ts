90d9d77507060dfd7d23476264768327003e0eb1
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-10-03 02:44:53 +0600

    refactor: Add nativeModule.ts for system shutdown and restart functionality

---
+++ nativeModule.ts
@@ -0,0 +1,22 @@
+import os from 'os';
+import { exec , spawn} from 'child_process';
+
+const restart = () => {
+    if (os.platform() === 'win32') {
+        exec('shutdown /r /t 0');
+    } else if (os.platform() === 'linux' || os.platform() === 'darwin') {
+        exec('sudo shutdown -r now');
+    } else {
+        throw new Error('Unsupported operating system');
+    }
+}
+
+const shutdown = () => {
+    if (os.platform() === 'win32') {
+        exec('shutdown /s /t 0');
+    } else if (os.platform() === 'linux' || os.platform() === 'darwin') {
+        exec('sudo shutdown -h now');
+    } else {
+        throw new Error('Unsupported operating system');
+    }
+}
